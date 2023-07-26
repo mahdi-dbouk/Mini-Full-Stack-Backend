1c0d20ca2b3fe750b7a08bb874de8b29e766be7a
Mahdi Dbouk <mahdi.dbouk97@gmail.com>
2023-07-26 12:25:26 +0300

    Stored query results in local variables

--- login.php
+++ login.php
@@ -13,6 +13,17 @@
     $stmt_get_user = "select * from users where email = ?";
     $get_user = $mysqli->prepare($stmt_get_user);
     $$get_user->bind_param('s',$email);
+    
+    $fname = null;
+    $lname = null;
+    $user_email = null;
+    $hashed_pass = null;
+    //excuting query
+    $get_user->execute();
+    $get_user->store_result();
+    $get_user->bind_result($fname, $lname, $user_email, $user_pass);
+    $get_user->fetch();
+
 
     
 ?>
